3a57aee8471e1154a68311f66d362030ab3298a0
Jeffrey Girard <me@jmgirard.com>
2023-10-07 21:22:14 -0500

    Add lavaan support

---
+++ R/utils.R
@@ -0,0 +1,49 @@
+# Generic utilities -------------------------------------------------------
+
+# Power operator for matrices
+"%^%" <- function(S, power) with(eigen(S), vectors %*% (values ^ power * t(vectors)))
+
+# Calculate ten berge factor scores
+#   From seminr package
+calc_ten_berge <- function(X, Lambda, Phi, i.means, i.sds) {
+  if (any(is.na(X))) {
+    # if any missing, impute using person average
+    p.means <- rowMeans(X, na.rm = TRUE)
+    missings <- which(is.na(X), arr.ind = TRUE)
+    X[is.na(X)] <- p.means[missings[, 1]]
+    X <- scale(X)
+  } else {
+    X <- t((t(X) - i.means) / i.sds)
+  }
+  R <- stats::cor(X, use = "pairwise")
+  R.sqrt.i <- R %^% -0.5
+  Phi.sqrt <- Phi %^% 0.5
+  L <- Lambda %*% Phi.sqrt
+  C <- R.sqrt.i %*% L %*% ((t(L) %*% chol2inv(chol(R)) %*% L) %^% -0.5)
+  W <- R.sqrt.i %*% C %*% Phi.sqrt
+  colnames(W) <- colnames(Lambda)
+  rownames(W) <- rownames(Lambda)
+  scores <- X %*% W
+  colnames(scores) <- colnames(Lambda)
+  scores
+}
+
+# lavaan utilities --------------------------------------------------------
+
+# Adapted from seminr package
+ten_berge_lavaan <- function(fit) {
+  x <- lavaan::lavInspect(fit, what = "data")
+  m <- colMeans(x)
+  s <- sqrt(diag(lavaan::lavInspect(fit, what = "sampstat")$cov))
+  lambda <- lavaan::lavInspect(fit, what = "std.lv")$lambda
+  phi <- matrix(lavaan::lavInspect(fit, what = "cor.lv"), ncol(lambda))
+  calc_ten_berge(x, lambda, phi, m, s)
+}
+
+make_lavaan_efa_syntax <- function(nfactors, varnames) {
+  paste0(
+    paste('efa("efa")*f', 1:nfactors, sep = "", collapse = " + "),
+    " =~ ",
+    paste(varnames, collapse = " + ")
+  )
+}
--- R/xba.R
+++ R/xba.R
@@ -1,5 +1,6 @@
 #' Extended Bass-Ackwards Analysis using psych
 #'
+#' @param data A data frame or matrix containing the raw data.
 #' @param r A correlation matrix (either as a matrix or data frame).
 #' @param nfactors The maximum number of factors to estimate.
 #' @param fm The factoring method from psych (default = `"minres"`).
@@ -11,17 +12,21 @@
 #' @export
 #'
 #' @examples
-#' xba_psych(forbes, 10)
-xba_psych <- function(r, nfactors, fm = "minres", rotate = "varimax",
-                      scores = "tenBerge", ...) {
+#' xba_psych(r = forbes, 10)
+xba_psych <- function(data = NULL, r = NULL, nfactors, fm = "minres",
+                      rotate = "varimax", scores = "tenBerge", ...) {
   # Assertions
-  stopifnot(is.matrix(r) || is.data.frame(r))
-  stopifnot(ncol(r) == nrow(r))
+  stopifnot(xor(is.null(data), is.null(r)))
+  stopifnot(is.null(data) || is.matrix(data) || is.data.frame(data))
+  stopifnot(is.null(r) || is.matrix(r) || is.data.frame(r))
+  stopifnot(is.null(r) || ncol(r) == nrow(r))
   stopifnot(is.numeric(nfactors))
   stopifnot(length(nfactors) == 1)
   stopifnot(is.finite(nfactors))
   stopifnot(nfactors >= 1)
   stopifnot(floor(nfactors) == ceiling(nfactors))
+  stopifnot(is.character(rotate))
+  stopifnot(length(rotate) == 1)
   fm <- match.arg(
     fm,
     choices = c(
@@ -29,50 +34,76 @@ xba_psych <- function(r, nfactors, fm = "minres", rotate = "varimax",
       "old.min", "alpha", "pca"
     )
   )
-  rotate <- match.arg(
-    rotate,
-    choices = c(
-      "none", "varimax", "quartimax", "bentlerT", "equamax", "varimin",
-      "geominT", "bifactor", "Promax", "promax", "oblimin", "simplimax",
-      "bentlerQ", "geominQ", "biquartimin", "cluster"
-    )
-  )
 
   # Coerce r to a matrix if needed
-  if (is.matrix(r) == FALSE) {
+  if (!is.null(r) && is.matrix(r) == FALSE) {
     r <- as.matrix(r)
   }
 
   # Preallocate vectors
   fits <- vector(mode = "list", length = nfactors)
+  loadings <- vector(mode = "list", length = nfactors)
+  fscores <- vector(mode = "list", length = nfactors)
+  weights <- vector(mode = "list", length = nfactors)
   corrs <- vector(mode = "list", length = nfactors)
   congs <- vector(mode = "list", length = nfactors)
 
   # Loop through number of factors from 1 to nfactors
   for (i in seq_along(fits)) {
+
     # Fit a factor analytic model to r with i factors and save results in fits
     if (fm == "pca") {
-      fits[[i]] <- psych::pca(
-        r = r,
-        nfactors = i,
-        rotate = rotate,
-        ...
-      )
+      if (!is.null(r)) {
+        fits[[i]] <- psych::pca(
+          r = r,
+          nfactors = i,
+          rotate = rotate,
+          ...
+        )
+      } else {
+        fits[[i]] <- psych::pca(
+          r = data,
+          nfactors = i,
+          rotate = rotate,
+          ...
+        )
+      }
     } else {
-      fits[[i]] <- psych::fa(
-        r = r,
-        nfactors = i,
-        fm = fm,
-        rotate = rotate,
-        scores = scores,
-        ...
-      )
+      if (!is.null(r)) {
+        fits[[i]] <- psych::fa(
+          r = r,
+          nfactors = i,
+          fm = fm,
+          rotate = rotate,
+          scores = scores,
+          ...
+        )
+      } else {
+        fits[[i]] <- psych::fa(
+          r = data,
+          nfactors = i,
+          fm = fm,
+          rotate = rotate,
+          scores = scores,
+          ...
+        )
+      }
+    }
+
+    # Extract the loadings and factor scores (or beta weights)
+    loadings[[i]] <- fits[[i]]$loadings
+    weights[[i]] <- fits[[i]]$weights
+    if (!is.null(data)) {
+      fscores[[i]] <- fits[[i]]$scores
     }
 
     # Rename the factors in loadings and weights as A1, B1:B2, C1:C3, etc.
     fnames <- paste0(LETTERS[[i]], 1:i)
-    colnames(fits[[i]]$loadings) <- fnames
-    colnames(fits[[i]]$weights) <- fnames
+    colnames(loadings[[i]]) <- fnames
+    colnames(weights[[i]]) <- fnames
+    if (!is.null(data)) {
+      colnames(fscores[[i]]) <- fnames
+    }
 
     if (i > 1) {
       # Preallocate inner lists for cross-level correlations and congruences
@@ -82,11 +113,16 @@ xba_psych <- function(r, nfactors, fm = "minres", rotate = "varimax",
       # Loop through all levels lowers than i
       for (j in 1:(i - 1)) {
         # Calculate factor correlations between levels i and j
-        corrs[[i]][[j]] <- t(fits[[j]]$weights) %*% r %*% fits[[i]]$weights
+        if (!is.null(r)) {
+          corrs[[i]][[j]] <- t(weights[[j]]) %*% r %*% weights[[i]]
+        } else {
+          corrs[[i]][[j]] <- stats::cor(fscores[[j]], fscores[[i]])
+        }
+
         # Calculate factor congruences between levels i and j
-        congs[[i]][[j]] <- psych::factor.congruence(
-          fits[[j]]$loadings,
-          fits[[i]]$loadings
+        congs[[i]][[j]] <- psych::fa.congruence(
+          loadings[[j]],
+          loadings[[i]]
         )
       }
     }
@@ -95,6 +131,9 @@ xba_psych <- function(r, nfactors, fm = "minres", rotate = "varimax",
   # Save results to output list
   out <- list(
     models = fits,
+    loadings = loadings,
+    weights = weights,
+    fscores = fscores,
     correlations = corrs,
     congruences = congs
   )
@@ -102,3 +141,88 @@ xba_psych <- function(r, nfactors, fm = "minres", rotate = "varimax",
   # Return output list
   out
 }
+
+#' Extended Bass-Ackwards Analysis using lavaan
+#'
+#' @param data A data frame or matrix containing the raw data.
+#' @param nfactors The maximum number of factors to estimate.
+#' @param rotate The factor rotation method from lavaan (default = `"varimax"`).
+#' @param ... Additional arguments to pass on to `lavaan::cfa()`.
+#'
+#' @return A list object with the basic results
+#' @export
+#'
+#' @examples
+#' data("PoliticalDemocracy", package = "lavaan")
+#' xba_lavaan(PoliticalDemocracy, nfactors = 3, rotate = "varimax")
+xba_lavaan <- function(data, nfactors, rotate = "varimax", ...) {
+
+  # Assertions
+  stopifnot(is.data.frame(data) || is.matrix(data))
+  stopifnot(is.numeric(nfactors))
+  stopifnot(length(nfactors) == 1)
+  stopifnot(is.finite(nfactors))
+  stopifnot(nfactors >= 1)
+  stopifnot(floor(nfactors) == ceiling(nfactors))
+  stopifnot(is.character(rotate))
+  stopifnot(length(rotate) == 1)
+
+  # Preallocate vectors
+  fits <- vector(mode = "list", length = nfactors)
+  loadings <- vector(mode = "list", length = nfactors)
+  fscores <- vector(mode = "list", length = nfactors)
+  corrs <- vector(mode = "list", length = nfactors)
+  congs <- vector(mode = "list", length = nfactors)
+
+  # Loop through number of factors from 1 to nfactors
+  for (i in seq_along(fits)) {
+    # Fit a factor analytic model to r with i factors and save results in fits
+    fits[[i]] <- lavaan::cfa(
+      model = make_lavaan_efa_syntax(i, colnames(data)),
+      data = data,
+      rotation = rotate,
+      ...
+    )
+
+    # Extract loadings and factor scores
+    loadings[[i]] <- lavaan::lavInspect(fits[[i]], what = "std.lv")$lambda
+    fscores[[i]] <- ten_berge_lavaan(fits[[i]])
+
+    # Rename the factors in loadings and weights as A1, B1:B2, C1:C3, etc.
+    fnames <- paste0(LETTERS[[i]], 1:i)
+    colnames(loadings[[i]]) <- fnames
+    colnames(fscores[[i]]) <- fnames
+
+    if (i > 1) {
+      # Preallocate inner lists for cross-level correlations and congruences
+      corrs[[i]] <- vector(mode = "list", length = i - 1)
+      congs[[i]] <- vector(mode = "list", length = i - 1)
+
+      # Loop through all levels lowers than i
+      for (j in 1:(i - 1)) {
+        # Calculate factor correlations between levels i and j
+        corrs[[i]][[j]] <- stats::cor(fscores[[i]], fscores[[j]])
+        # Calculate factor congruences between levels i and j
+        congs[[i]][[j]] <- psych::fa.congruence(
+          loadings[[j]],
+          loadings[[i]]
+        )
+      }
+    }
+  }
+
+  # Save results to output list
+  out <- list(
+    models = fits,
+    loadings = loadings,
+    weights = weights,
+    fscores = fscores,
+    correlations = corrs,
+    congruences = congs
+  )
+
+  # Return output list
+  out
+
+}
+
